f8b4f9ebf0bc377737613f4bb4b386b88781528c
Mariree003 <139806210+Mariree003@users.noreply.github.com>
2025-05-17 17:14:06 -0300

    Update Cadastro.js

--- avaliador-vizinhanca/src/pages/Cadastro.js
+++ avaliador-vizinhanca/src/pages/Cadastro.js
@@ -8,28 +8,20 @@ export default function Cadastro() {
   const handleCadastro = (e) => {
     e.preventDefault();
 
-    // Pega os usuários já cadastrados no localStorage (ou cria um array vazio)
     const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
-    // Verifica se o email já está cadastrado
+    // Verifica se o email já existe
     const emailExiste = usuarios.some((user) => user.email === email);
-
     if (emailExiste) {
-      alert('Este email já está cadastrado. Por favor, faça login.');
+      alert('Esse email já está cadastrado.');
       return;
     }
 
-  
     usuarios.push({ email, senha });
-
-    // Salva novamente no localStorage
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
 
-    alert('Cadastro realizado com sucesso! Agora faça login.');
-
-  
-    setEmail('');
-    setSenha('');
+    alert('Cadastro realizado com sucesso!');
+    window.location.href = '/login';
   };
 
   return (
