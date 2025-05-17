e63bd9048405bfb38477eaa747557bc6763a9419
Mariree003 <139806210+Mariree003@users.noreply.github.com>
2025-05-17 17:06:19 -0300

    Update App.js

--- avaliador-vizinhanca/src/App.js
+++ avaliador-vizinhanca/src/App.js
@@ -1,6 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -11,34 +8,15 @@ import Bairro from './pages/Bairro';
 
 function App() {
   return (
-
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/avaliar" element={<Avaliar />} />
-        <Route path="/bairro/:id" element={<Bairro />} />
+        <Route path="/bairro" element={<Bairro />} />
       </Routes>
     </BrowserRouter>
-
-    
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
